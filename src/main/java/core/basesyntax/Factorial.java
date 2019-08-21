c1688e09ee71a553fad9ec115c140c63151a5ffb
Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>
2019-08-21 17:56:21 +0300

    changed method to recursive

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -4,11 +4,15 @@ package core.basesyntax;
  * <p>Реализуйте метод getFactorial(int number) который вовзращает факториал числа.</p>
  */
 public class Factorial {
+
     public int getFactorial(int number) {
-        int factorial = 1;
-        for (int i = 1; i <= number; i++) {
-            factorial *= i;
+        int factorial;
+
+        if (number == 1 || number == 0) {
+            return 1;
         }
+
+        factorial = getFactorial(number - 1) * number;
         return factorial;
     }
 }
