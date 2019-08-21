096ac478a898add8ce0bf4717980be788d669fb9
Kokhanevich <nike.k1995@gmail.com>
2019-08-21 16:06:39 +0300

    added solution for HW3-factorial

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -5,6 +5,9 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        return 0;
+        if (number == 0 || number == 1) {
+            return 1;
+        }
+        return number * getFactorial(number - 1);
     }
 }
