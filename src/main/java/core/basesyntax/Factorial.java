c5afe061dd976962523eb425878994a8b278e596
Anton Kozhukhovsky <a.kozhukhovsky@gmail.com>
2019-08-22 13:25:28 +0300

    refactored

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -6,13 +6,9 @@ package core.basesyntax;
 public class Factorial {
 
     public int getFactorial(int number) {
-        int factorial;
-
         if (number == 1 || number == 0) {
             return 1;
         }
-
-        factorial = getFactorial(number - 1) * number;
-        return factorial;
+        return getFactorial(number - 1) * number;
     }
 }
