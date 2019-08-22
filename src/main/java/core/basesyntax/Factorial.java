d195784d23c4bf2a38b4d76c1bed992d4dc7fe2b
Ruslan <vip.russle@gmail.com>
2019-08-22 10:47:19 +0300

    added changes in code

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -5,8 +5,13 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        return number = (number == 1) ? number : (number == 0) ? 1
-            : number * getFactorial(number - 1);
+        if (number == 1) {
+            return number;
+        } else if (number == 0) {
+            return  1;
+        } else {
+            return number * getFactorial(number - 1);
+        }
     }
 
 }
