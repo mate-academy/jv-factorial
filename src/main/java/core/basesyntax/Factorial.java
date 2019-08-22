2369392669d57e7d2b170b78d94bdfc70a08eed8
Rosinskyi <sergey22061993@gmail.com>
2019-08-22 13:10:40 +0300

    fixed variable name

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -5,11 +5,10 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        int n = 1;
-        for (int i = 1; i <= number; i++) {
-            n *= i;
+        int value = 1;
+        for (int i = 2; i <= number; i++) {
+            value *= i;
         }
-        return n;
+        return value;
     }
-
 }
