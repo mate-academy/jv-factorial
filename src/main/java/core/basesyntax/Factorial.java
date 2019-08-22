d8f5ddb6894b5f7abafb957dda43b04a405dbb0f
Саша Бунин <sasha192.bunin@gmail.com>
2019-08-22 19:59:33 +0300

    Update Factorial.java

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -4,8 +4,6 @@ package core.basesyntax;
  * <p>Реализуйте метод getFactorial(int number) который вовзращает факториал числа.</p>
  */
 public class Factorial {
-
-
     public int getFactorial(int number) {
         if (number == 0) {
             return 1;
