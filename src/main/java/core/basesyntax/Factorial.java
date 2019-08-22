653972b1f8eb8a339a3dc51c78c224c50244eb12
lenovo <suleimanovayev@gmail.com>
2019-08-22 14:37:15 +0300

    fixing task

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -5,6 +5,6 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        return 0;
+        return (number == 1 || number == 0) ? 1 : number * getFactorial(number - 1);
     }
 }
