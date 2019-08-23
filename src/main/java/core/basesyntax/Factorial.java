1b9847100c3ef091de7b29998b842d767cfb91ed
Ruslan <vip.russle@gmail.com>
2019-08-23 13:43:06 +0300

    added one statement

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -5,11 +5,9 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        if (number == 0 || number == 1) {
-            return 1;
-        } else {
+        if (number > 1) {
             return number * getFactorial(number - 1);
         }
+        return 1;
     }
-
 }
