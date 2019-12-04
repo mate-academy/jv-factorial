aa6ad8c2eda2160fc249eaba213deaba90fa50cc
Julianevinska <57527101+Julianevinska@users.noreply.github.com>
2019-12-04 17:22:11 +0200

    HW 3.4 - fixed solution

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -6,10 +6,9 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        int result = 1;
-        for (int i = 1; i <= number; i++) {
-            result = result * i;
+        if ((number == 0) || (number == 1)) {
+            return 1;
         }
-        return result;
+        return getFactorial(number - 1) * number;
     }
 }
