cff4bdb74ab3eb46b08b069e6b1b9b7930f06296
Alexandr Sashchenko <santys87@gmail.com>
2019-12-05 12:45:43 +0200

    Mate HW 3.4 solution added (updated)

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -6,7 +6,7 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        if (number <= 1) {
+        if (number >= 0 && number <= 1) {
             return 1;
         }
         return number * getFactorial(number - 1);
