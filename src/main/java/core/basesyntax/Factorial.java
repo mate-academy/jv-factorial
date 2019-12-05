c48b5d1218a0d27aa8955328cb6561224714df3c
Dmitrii <zinchukdimaa@gmail.com>
2019-12-05 08:53:03 +0200

    added recursion for getFactorial method

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -6,10 +6,9 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        int factorial = 1;
-        for (int i = 1; i <= number; i++) {
-            factorial *= i;
+        if (number == 1 || number == 0) {
+            return 1;
         }
-        return factorial;
+        return number * getFactorial(number - 1);
     }
 }
