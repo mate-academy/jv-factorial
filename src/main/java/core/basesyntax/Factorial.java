b7e4416adcb1b0736a1ef91a8809b0b63eb07ad6
Andrey Tanchak <61573898+boterf@users.noreply.github.com>
2020-04-04 00:46:40 +0300

    some changes

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Factorial {
 
     public int getFactorial(int number) {
-   return number == 0|| number == 1 ? 1
-       : number * getFactorial(number - 1);
+        return number == 0 || number == 1 ? 1
+            : number * getFactorial(number - 1);
     }
 }
