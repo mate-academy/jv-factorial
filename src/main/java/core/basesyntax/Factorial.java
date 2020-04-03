8427629529748149a6a0a15233ef73009599815a
Andrey Tanchak <61573898+boterf@users.noreply.github.com>
2020-04-03 23:56:55 +0300

    added "true" solution

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -3,14 +3,7 @@ package core.basesyntax;
 public class Factorial {
 
     public int getFactorial(int number) {
-        if (number == 1 || number == 0) {
-            return 1;
-        }
-        int[] abc = new int [number];
-        for (int i = 1; i <= abc.length; i++) {
-            abc[abc.length - i] = number;
-            number--;
-        }
-        return abc[abc.length - 1] * getFactorial(abc[abc.length - 2]);
+   return number == 0|| number == 1 ? 1
+       : number * rec(number - 1);
     }
 }
