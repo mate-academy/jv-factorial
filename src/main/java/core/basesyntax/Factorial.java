374976fb5ae6eda75824cc2742d27a104fdf791e
maks45 <maksim.durov45@gmail.com>
2020-04-03 13:55:01 +0300

    added solution

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -6,6 +6,6 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        return 0;
+        return number < 2 ? 1 : number * getFactorial(--number);
     }
 }
