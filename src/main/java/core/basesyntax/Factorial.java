ee13746d25680365c4def6a3ceb7e03b7be9f19f
NastiaFasova <fasovaaa@fmail.com>
2020-04-03 12:45:09 +0300

    Added solution to the 4-th task of HW-6

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -6,6 +6,7 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        return 0;
+        return number == 1 || number == 0 ? 1
+                : number * getFactorial(number - 1);
     }
 }
