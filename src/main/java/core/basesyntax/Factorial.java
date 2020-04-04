1779896e7c7354d31d6ffff86d2bc9abf93b01d4
unknown <bavikaalexey@gmail.com>
2020-04-04 22:50:24 +0300

    added solution

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -5,7 +5,7 @@ package core.basesyntax;
  * Реализовать с помощью рекурсии.</p>
  */
 public class Factorial {
-    public int getFactorial(int number) {
-        return 0;
+    public static int getFactorial(int number) {
+        return (number == 0 || number == 1) ? 1 : number * getFactorial(number - 1);
     }
 }
