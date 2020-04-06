a32d486025557b14c18a579fdfadbc8cb4b3085a
xeddin <61269748+xeddin@users.noreply.github.com>
2020-04-06 10:32:27 +0300

    Deleted redundunt variable

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -6,11 +6,9 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        int result = 1;
         if (number == 1 || number == 0) {
-            return result;
+            return 1;
         }
-        result = number * getFactorial(number - 1);
-        return result;
+        return number * getFactorial(number - 1);
     }
 }
