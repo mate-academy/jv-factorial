9d22709216c221da44b1931284c1feb3efad1067
Kostian <pyteec.ua@i.ua>
2020-08-03 22:11:27 +0300

    added solution

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -6,6 +6,12 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        return 0;
+        if (number == 0) {
+            return 1;
+        }
+        if (number == 1) {
+            return 1;
+        }
+        return number * getFactorial(number - 1);
     }
 }
