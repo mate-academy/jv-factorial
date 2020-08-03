172f16d6925a43e7b4d12de67149bb3d5fe6a1ef
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-08-03 16:48:31 +0300

    added solution

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -1,11 +1,17 @@
 package core.basesyntax;
 
 /**
- * <p>Реализуйте метод getFactorial(int number) который вовзращает факториал числа.
+ * <p>Реализуйте метод getFactorial(int number) который вовзращает факториал
+ * числа.
  * Реализовать с помощью рекурсии.</p>
  */
 public class Factorial {
     public int getFactorial(int number) {
-        return 0;
+        int result = 1;
+        if (number == 1 || number == 0) {
+            return result;
+        }
+        result = number * getFactorial(number - 1);
+        return result;
     }
 }
