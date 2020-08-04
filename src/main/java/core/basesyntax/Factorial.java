e71fce5d3f1f3bddf588493aa82ea2860d01cdfb
Dubovaya <dubovaya.java@gmail.com>
2020-08-04 12:20:00 +0300

    redundant lines deleted

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -2,12 +2,7 @@ package core.basesyntax;
 
 public class Factorial {
     public int getFactorial(int number) {
-
-        int result = 1;
-        if (number == 1 || number == 0) {
-            return result;
-        }
-        result = number * getFactorial(number - 1);
-        return result;
+        return (number == 0 || number == 1) ? 1 :
+                number * getFactorial(number - 1);
     }
 }
