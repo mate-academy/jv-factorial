1c492106d9928270c538ba88edea261b5603673c
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-08-04 11:31:34 +0300

    one condition update

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -7,7 +7,6 @@ package core.basesyntax;
 public class Factorial {
     public int getFactorial(int number) {
         return number == 0 ? 1 :
-                number == 1 ? 1 :
                         number * getFactorial(number - 1);
     }
 }
