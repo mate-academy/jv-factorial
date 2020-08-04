33e4849fece2af8e0e00eb0929e44218508d6e46
YaTut1901 <59802427+YaTut1901@users.noreply.github.com>
2020-08-04 08:39:37 +0300

    hw

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -6,12 +6,9 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        if (number == 0) {
+        if (number == 0 || number == 1) {
             return 1;
-        } else if (number == 1) {
-            return 1;
-        } else {
-            return number * getFactorial(number - 1);
         }
+        return number * getFactorial(number - 1);
     }
 }
