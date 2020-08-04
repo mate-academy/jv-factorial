f84bdf525cf37ffd6828ee74c114ccf921524c2d
margarita-ignatieva <ignatevamargarita950@gmail.com>
2020-08-04 10:05:54 +0300

    changes made

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -3,7 +3,7 @@ package core.basesyntax;
 public class Factorial {
     public int getFactorial(int number) {
         int result = 1;
-        if (number == 1 || number == 0) {
+        if (number <= 1) {
             return 1;
         }
         result = number * getFactorial(number - 1);
