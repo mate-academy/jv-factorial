30d061abe776c8eed10fdb951cc2a47b0c31267b
Ilya.Kvitkovskyi <ilya.kvitkovskyi@gmail.com>
2020-08-04 11:11:42 +0300

    simplify to one condition

--- src/main/java/core/basesyntax/Factorial.java
+++ src/main/java/core/basesyntax/Factorial.java
@@ -6,7 +6,7 @@ package core.basesyntax;
  */
 public class Factorial {
     public int getFactorial(int number) {
-        return (number == 0 || number == 1) ? 1
+        return (number == 0) ? 1
                 : number * getFactorial(number - 1);
     }
 }
